bb2f1901e89ac4fd448252ad983a3cfb39a1cb2e
prasetyo putra <prasetyo531@gmail.com>
2018-10-29 13:45:35 +0700

    classmovement

--- Automation-Master/src_test/tc_prodDetail/commentLastAddedProduct.java
+++ Automation-Master/src_test/tc_prodDetail/commentLastAddedProduct.java
@@ -1,4 +1,4 @@
-package testcase;
+package tc_prodDetail;
 
 import static org.testng.Assert.assertTrue;
 
@@ -89,7 +89,7 @@ public class commentLastAddedProduct extends controller {
 		checkoutPage checkout = new checkoutPage(driver);
 		
 		prop= new Properties();
-		FileInputStream fis=new FileInputStream("//Users//mac//Documents//Automation//mavenjob//Automation-Master//src_controller//resources//data.properties");
+		FileInputStream fis=new FileInputStream(workingDir+"//src_controller//resources//data.properties");
 		prop.load(fis);
 		String testenv=prop.getProperty("testlocation");
 		
@@ -300,7 +300,7 @@ public class commentLastAddedProduct extends controller {
 	@DataProvider	  
 	public Object[][] existingCust() throws Exception {
 	     
-		FileInputStream filepath = new FileInputStream("//Users//mac//Documents//Automation//mavenjob//Automation-Master//Workbook1.xls");
+		FileInputStream filepath = new FileInputStream(workingDir+"//Workbook1.xls");
 
 		Workbook wb = Workbook.getWorkbook(filepath);
 		Sheet sheet = wb.getSheet("existing");
