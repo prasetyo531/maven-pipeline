cbc1d512bf16fe1ced5a8e43ac4e60a50f3cb5b3
prasetyoputra <myjne001@gmail.com>
2019-06-01 15:21:25 +0700

    progress-target

--- Automation-Master/src_test/tc_addProduct/addProductBeforeLogin.java
+++ Automation-Master/src_test/tc_addProduct/addProductBeforeLogin.java
@@ -2,15 +2,18 @@ package tc_addProduct;
 
 import static org.testng.Assert.assertTrue;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.CellType;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -26,9 +29,12 @@ import org.testng.annotations.Test;
 
 import assertObject.assertAddProduct;
 import assertObject.assertHome;
-import jxl.Cell;
-import jxl.Sheet;
-import jxl.Workbook;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import pageObjects.cartPage;
 import pageObjects.categoryPage;
 import pageObjects.checkoutPage;
@@ -40,118 +46,123 @@ import resources.Controller;
 import resources.support;
 
 public class addProductBeforeLogin extends Controller {
-	
-public static Logger log =LogManager.getLogger(support.class.getName());
-	
-	public static RemoteWebDriver driver= null;
-	public static WebElement main= null;
-	public static Properties prop=null;
-	
-	public String UrlLogin = null;
-	
-	@BeforeTest
-	@Parameters({ "browser" })
-	public void setUp(String browser) throws IOException {
-		System.out.println("*******************");
-		driver = Controller.getDriver(browser);
-		
-	}
-	
-	@Test(dataProvider="existingCust")
-	public void scenario_satu(String email,String password,String alamat,String telepon) throws Exception {
-		
-		support supp= new support();
-		homepage home = new homepage(driver);
-		login logpro = new login(driver);
-		addproductpage productpage = new addproductpage(driver);
-		
-		categoryPage cat = new categoryPage(driver);
-		ProductPage prod = new ProductPage(driver);
-		cartPage cpage = new cartPage(driver);
-		checkoutPage checkout = new checkoutPage(driver);
-		
-		assertHome asser = new assertHome(driver);
-		assertAddProduct asserAddProd = new assertAddProduct(driver);
-		
-		prop= new Properties();
-		FileInputStream fis=new FileInputStream(workingDir+"//src_controller//resources//data.properties");
-		prop.load(fis);
-		String testenv=prop.getProperty("testlocation");
-		
-		if(testenv.equalsIgnoreCase("prod")){
-        	driver.navigate().to("http://femaledaily.com/");  //https://dev.uangteman.com/a/NHeHv
-             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        	} else {
-        		driver.navigate().to("http://femaledaily.net/");  //https://dev.uangteman.com/a/NHeHv
-                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        	}
 
-		driver.manage().window().setSize(new Dimension(1650, 1200));
+    public static Logger log = LogManager.getLogger(support.class.getName());
 
-		//click hamburger
-		home.Hamburger().click();
-		
-		home.clickMenuReview().click();
+    public static RemoteWebDriver driver = null;
+    public static WebElement main = null;
+    public static Properties prop = null;
+
+    public String UrlLogin = null;
+
+    @BeforeTest
+    @Parameters({"browser"})
+    public void setUp(String browser) throws IOException {
+        System.out.println("*******************");
+        driver = Controller.getDriver(browser);
+
+    }
+
+    @Test(dataProvider = "existingCust")
+    public void scenario_satu(String email, String password, String alamat, String telepon) throws Exception {
+
+        support supp = new support();
+        homepage home = new homepage(driver);
+        login logpro = new login(driver);
+        addproductpage productpage = new addproductpage(driver);
+
+        categoryPage cat = new categoryPage(driver);
+        ProductPage prod = new ProductPage(driver);
+        cartPage cpage = new cartPage(driver);
+        checkoutPage checkout = new checkoutPage(driver);
+
+        assertHome asser = new assertHome(driver);
+        assertAddProduct asserAddProd = new assertAddProduct(driver);
+
+        prop = new Properties();
+        FileInputStream fis = new FileInputStream(workingDir + "//src_controller//resources//data.properties");
+        prop.load(fis);
+        String testenv = prop.getProperty("testlocation");
+
+        if (testenv.equalsIgnoreCase("prod")) {
+            driver.navigate().to("http://femaledaily.com/");  //https://dev.uangteman.com/a/NHeHv
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        } else {
+            driver.navigate().to("http://femaledaily.net/");  //https://dev.uangteman.com/a/NHeHv
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        }
+
+        driver.manage().window().setSize(new Dimension(1650, 1200));
+
+        //click hamburger
+        home.Hamburger().click();
+
+        home.clickMenuReview().click();
 //		asser.waitNewestReview();
-		
-		WebElement getmenu= home.getAddProduct(); //xpath megamenu nya  
-		Actions act = new Actions(driver);
-		act.moveToElement(getmenu).perform();
-		
-//		asser.addproducttodisplay();
 
-//        WebElement clickElement= home.clickAddProduct(); //xpath sub megamenu nya
-//		act.moveToElement(clickElement).click().perform();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement getmenu = home.getAddProduct(); //xpath megamenu nya
+        Actions act = new Actions(driver);
+        act.moveToElement(getmenu).perform();
+
+        asser.addproducttodisplay();
+
+        WebElement clickElement= home.clickAddProduct(); //xpath sub megamenu nya
+		act.moveToElement(clickElement).click().perform();
+
+		Thread.sleep(3000);
 //
-//		UrlLogin = driver.getCurrentUrl();
-//		assertTrue(UrlLogin.contains("http://account.femaledaily"));
-		
-		
-	}
-	
-	@AfterMethod
-	public void tearDown() {
-		if(driver!=null) {
-			System.out.println("Closing browser");
-			driver.close();
-		}
-	}
-	
-	public void ExtractJSLogs() {
+		UrlLogin = driver.getCurrentUrl();
+		assertTrue(UrlLogin.contains("http://account.femaledaily"));
+
+        logpro.fillusername().sendKeys(email);
+        logpro.fillpassword().sendKeys(password);
+        logpro.clickbuttonlogin().click();
+
+        Thread.sleep(3000);
+
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            System.out.println("Closing browser");
+            driver.close();
+        }
+    }
+
+    public void ExtractJSLogs() {
         LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
         for (LogEntry entry : logEntries) {
             System.out.println(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage());
         }
     }
-	
-	@DataProvider	  
-	public Object[][] existingCust() throws Exception {
-	     
+
+    @DataProvider
+    public Object[][] existingCust() throws Exception {
+
         FileInputStream filepath = new FileInputStream(workingDir+"//Workbook1.xls");
-        //InputStream filepath = getClass().getResourceAsStream(workingDir+"//Workbook1.xls");
 
-		Workbook wb = Workbook.getWorkbook(filepath);
-		Sheet sheet = wb.getSheet("existing");
+        Workbook wb = Workbook.getWorkbook(filepath);
+        Sheet sheet = wb.getSheet("existing");
 
-		int row = sheet.getRows();
-		System.out.println("number of rows"+row);
-		int column = sheet.getColumns();
-		System.out.println("number of columns"+column);
-		String Testdata[][] = new String[row-1][column];
-		int count=0;
+        int row = sheet.getRows();
+        System.out.println("number of rows"+row);
+        int column = sheet.getColumns();
+        System.out.println("number of columns"+column);
+        String Testdata[][] = new String[row-1][column];
+        int count=0;
 
-		     for (int i = 1; i < row; i++)
-		     	{
-		    	 for (int j = 0; j < column; j++)
-		    	 {
-		    		 Cell cell = sheet.getCell(j, i);
-		    		 Testdata[count][j] = cell.getContents();
-		     	}
-		    	 count++;
-		       }
-		     filepath.close();
-		     return Testdata;
-		     }
+        for (int i = 1; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                Cell cell = sheet.getCell(j, i);
+                Testdata[count][j] = cell.getContents();
+            }
+            count++;
+        }
+        filepath.close();
+        return Testdata;
+    }
 }
-
