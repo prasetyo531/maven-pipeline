c037be4213f4dca260eac717b7a16e09b1030724
FDN-Prasetyo <prasetyo531@gmail.com>
2019-08-14 17:10:45 +0700

    no message

--- Automation-Master/target/healthcheck.sh
+++ Automation-Master/target/healthcheck.sh
@@ -4,13 +4,15 @@
 # BROWSER
 # MODULE
 
-echo "Checking if hub is ready - 10.10.10.241"
+#echo "Checking if hub is ready - 10.10.10.12"
+echo "Checking if hub is ready - $HUB_HOST"
 
-while [ "$( curl -s http://localhost:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
 	sleep 1
 done
 
 # start the java command
 java -cp seleniumDocker-1.0.0.jar:seleniumDocker-1.0.0-tests.jar:libs/*:classes:*resources/* \
+    DHUB_HOST=$HUB_HOST \
     org.testng.TestNG $MODULE
